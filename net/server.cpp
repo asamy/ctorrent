1502872870d7f2cd909ec1c65279c104e63a9fe6
A. Samy <f.fallen45@gmail.com>
2015-11-13 12:02:09 +0000

    add server

---
+++ net/server.cpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2013-2015 Ahmed Samy  <f.fallen45@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "server.h"
+
+Server::Server(uint16_t port)
+	: m_acceptor(g_service, asio::ip::tcp::endpoint(asio::ip::tcp::v4(), port))
+{
+}
+
+Server::~Server()
+{
+}
+
+void Server::stop()
+{
+	m_acceptor.cancel();
+	m_acceptor.close();
+}
+
+void Server::accept(const Acceptor &ac)
+{
+	Connection *conn = new Connection();
+	m_acceptor.async_accept(conn->m_socket,
+		[=] (const boost::system::error_code &error) {
+			if (error)
+				delete conn;
+			else
+				ac(conn);
+		}
+	);
+}
+
